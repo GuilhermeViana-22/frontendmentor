9cbf78e1571db9e4ba4c1157a9716ccb4b9dc1e6
GuilhermeViana-22 <gguicido.viana@gmail.com>
2021-08-19 23:58:58 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,401 @@
+let total ,ValorTaxa;
+
+function myFunction(value1 , value2){
+ 
+    ValorTaxa = parseFloat(value1);
+
+    console.log(ValorTaxa);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction0() {
+    
+
+ 
+
+//     const  
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+
+//     console.log("numero de pessoas é " + qtdPessoas);
+
+//     document.querySelector("#btn").addEventListener("click", tip())
+
+//     function tip() {
+
+//         let value = document.getElementById("demo").innerHTML = "0.05";
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * 5) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+//     }
+// }
+// /////////////////////////////////////////
+// function myFunction2() {
+
+//     let total = '';
+
+//     let ValorTaxa = '';
+
+//     let numberofPeople = '';
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+//     console.log("numero de pessoas é " + qtdPessoas);
+
+//     document.querySelector("#btn2").addEventListener("click", tip2())
+
+//     function tip2() {
+
+//         let value = document.getElementById("demo").innerHTML = "0.10";
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * 10) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+
+//     }
+// }
+// ///////////////////////////////////////
+
+// function myFunction3() {
+
+//     let total = '';
+
+//     let ValorTaxa = '';
+
+//     let numberofPeople = '';
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+//     console.log("numero de pessoas é " + qtdPessoas);
+
+//     document.querySelector("#btn2").addEventListener("click", tip3())
+
+//     function tip3() {
+
+//         let value = document.getElementById("demo").innerHTML = "0.15";
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * 15) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+
+//     }
+// }
+// /////////////////////////////////////////////
+// function myFunction4() {
+
+//     let total = '';
+
+//     let ValorTaxa = '';
+
+//     let numberofPeople = '';
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+//     console.log("numero de pessoas é " + qtdPessoas);
+
+//     document.querySelector("#btn4").addEventListener("click", tip4())
+
+//     function tip4() {
+
+//         let value = document.getElementById("demo").innerHTML = "0.25";
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * 25) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+
+//     }
+// }
+
+// function myFunction5() {
+
+//     let total = '';
+
+//     let ValorTaxa = '';
+
+//     let numberofPeople = '';
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+ 
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+//     console.log("numero de individual é " + qtdtip);
+
+//     document.querySelector("#btn5").addEventListener("click", tip5())
+
+//     function tip5() {
+
+//         let value = document.getElementById("demo").innerHTML = "0.50";
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * 50) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+
+//     }
+// }
+
+// function myFunction6() {
+
+//     let total = '';
+
+//     let ValorTaxa = '';
+
+//     let numberofPeople = '';
+
+//     var input = document.querySelector("#valueBill");
+
+//     var valor = input.value;
+//     ////
+//     var Custom = document.querySelector("#Custom");
+
+//     var qtdtip = Custom.value;
+//     ///
+//     var numberp = document.querySelector("#lname");
+
+//     var qtdPessoas = numberp.value;
+
+//     console.log("numero de pessoas é " + qtdPessoas);
+
+//     document.querySelector("#btn5").addEventListener("click", tip6())
+
+//     function tip6() {
+
+//         let value = document.getElementById("demo").innerHTML = qtdtip/100;
+
+//         if (isNaN(value)) {
+
+//             alert("value não é um numero");
+
+//             console.log(value);
+
+//         } else if (qtdPessoas == 0) {
+
+//             let msg = document.getElementById("message").style.color = 'red';
+
+//             msg = document.getElementById("message").innerHTML = "Its necessary to inform the numbe of the people";
+
+//             numberp.style = 'background-color: #e7c5c5';
+
+//             let msg1 = document.getElementById("resultado").style.color = '#c5e4e7';
+
+//             msg1 = document.getElementById("resultado").innerHTML = "ERROR";
+
+//             console.log("esta gerando um 0");
+//         } else {
+
+//             numberp.style = 'background-color: #d2fafac9';
+
+//             msg = document.getElementById("message").innerHTML = "";
+
+//             ValorTaxa = (valor * qtdtip) / 100; // valor da taxa a ser pago
+
+//             total = (parseFloat(valor) + ValorTaxa);
+
+//             total = total / qtdPessoas
+
+//             document.getElementById("resultado").innerHTML = total;
+//         }
+//         console.log(valor);
+
+//     }
+// }
+
+
+
